d63959c172441311b881403440263e88aa03ed50
Benjamin Daniel <postanbenny@gmail.com>
2019-11-15 20:31:09 +0100

    Added comments

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,25 +4,48 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    // Ball velocity when the ball get's launched.
     public float ballInitalVelocity = 600f;
 
+    // Rigidbody component of the ball.
     private Rigidbody rb;
+    // Is the ball currently in action?
     private bool ballInPlay = false;
 
-    // Start is called before the first frame update
+    // Awake get's called even before Start()
     void Awake()
     {
+        // Get's the Rigidbody component from the Ball GameObject
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        /// Input.GetButtonDown("Fire1") = Fire button get's pressed down 
+        /// (here left mouse button)
+
+        // Ball should only get fired when paddle get's reset
         if (Input.GetButtonDown("Jump") && ballInPlay == false)
         {
-            ballInPlay = true;
+            /// Make the ball independent from the paddle, so the ball position
+            /// doesn't change when the paddle position changes
             transform.parent = null;
+            // Ball is now in action.
+            ballInPlay = true;
+            /// Usally when working with physics values you would use 
+            /// FixedUpdate (get's called in a fixed update loop, 
+            /// with same time difference), but here it only get's called once
+
+            /// Ball physics can now be affected by other collision objects.
+            /// Be careful with isKinematic, as you have to think the other way
+            /// around. So isKinematic = true => Ball get's controlled by script,
+            /// isKinematic = false => Ball get's affected (and controlled) by physics.
             rb.isKinematic = false;
+            // Vector can be a point, but also a direction, that we use here
+
+            /// Adds a directional force to the ball. The direction is diagonally
+            /// as we add force to the x and y direction.
             rb.AddForce(new Vector3(ballInitalVelocity, ballInitalVelocity, 0f));
         }
     }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    /// public-variables are accessable from the inspector in the Unity-Editor,
+    /// from other classes and the class itself.
+
+    // paddleSpeed stores the value for how fast the paddle moves.
     public float paddleSpeed = 1f;
+
+    /// Writing private is optional as they are private by default, 
+    /// just makes it more clear.
+
+    /// Vector3(x, y, z) = a vector in space with x-y-z-coordinates
+
+    // playerPos stores the value for the next player position.
     private Vector3 playerPos = new Vector3(0, -9.5f, 0);
+
+    // NOTE: Yes methods are written with a capital letter in front according to Unity's coding patterns.
 
     // Update is called once per frame
     void Update()
     {
+        /// Input.GetAxis("Horizontal") = Input from the LEFT or RIGHT arrow keys
+        /// or from the "A" or "D" keys
+
+        /// transform.position.x returns the x-value for the paddles position
+        /// of it's transform component.
         float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        /// Mathf.Clamp(x, min, max) = the value x is always between the min and 
+        /// max values.
+
+        /// playerPos get's updated here to the new calculated position, with
+        /// x changing with user input, y and z staying always the same.
         playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);
+        /// Set the current paddle position = transform.position to the
+        /// new position = playerPos.
         transform.position = playerPos;
     }
 }
