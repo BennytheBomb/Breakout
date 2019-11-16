1d8d77c3a0a57c07fcecf9c8b381c39b2769bae7
Benjamin Daniel <postanbenny@gmail.com>
2019-11-16 17:09:51 +0100

    Final result

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int lives = 3;
+    public int bricks = 20;
+    public float resetDelay = 1f;
+    public Text livesText;
+    public GameObject gameOver;
+    public GameObject youWon;
+    public GameObject bricksPrefab;
+    public GameObject paddle;
+    public GameObject deathParticles;
+
+    public static GameManager instance = null;
+
+    private GameObject clonePaddle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Setup();
+    }
+
+    void Setup()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity);
+        Instantiate(bricksPrefab, transform.position, Quaternion.identity);
+    }
+
+    void CheckGameOver()
+    {
+        if (bricks < 1)
+        {
+            youWon.SetActive(true);
+            Time.timeScale = 0.25f;
+            Invoke("Reset", resetDelay);
+        } else if (lives < 1)
+        {
+            gameOver.SetActive(true);
+            Time.timeScale = 0.25f;
+            Invoke("Reset", resetDelay);
+        }
+    }
+
+    void Reset()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoseLife()
+    {
+        lives--;
+        livesText.text = "Lives: " + lives;
+        Instantiate(deathParticles, clonePaddle.transform.position, Quaternion.identity);
+        Destroy(clonePaddle);
+        Invoke("SetupPaddle", resetDelay);
+        CheckGameOver();
+    }
+
+    void SetupPaddle()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity);
+    }
+
+    public void BrickDestroyed()
+    {
+        bricks--;
+        CheckGameOver();
+    }
+}
